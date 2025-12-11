c75dc6cec717695d76a7ee807b993cee9ae4b46d
Daniel Roja <daniel.red.jb@gmail.com>
2025-12-11 02:30:14 -0600

    add ProductosModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TareasModule } from './tareas/tareas.module';
 
+import { ProductosModule } from './productos/productos.module';
+
 
 @Module({
-  imports: [TareasModule],
+  imports: [TareasModule, ProductosModule],
   controllers: [AppController],
   providers: [AppService],
 })
