1232717c0e49ac6db0353842ddbe33552be04b09
Daniel Roja <daniel.red.jb@gmail.com>
2025-12-11 20:37:56 -0600

    add global configuration and TypeORM setup for MySQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,31 @@ import { AppService } from './app.service';
 import { TareasModule } from './tareas/tareas.module';
 
 import { ProductosModule } from './productos/productos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GatosModule } from './gatos/gatos.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [TareasModule, ProductosModule],
+  imports: [
+    
+    ConfigModule.forRoot({isGlobal: true}),
+
+    TypeOrmModule.forRoot({
+      type:"mysql",
+      host:"localhost",
+      port:3307,
+      username:"user_crud",
+      password:"root",
+      database:"db_crud",
+      autoLoadEntities:true,
+      synchronize: true
+    }),
+    
+    TareasModule, 
+    ProductosModule, GatosModule
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
